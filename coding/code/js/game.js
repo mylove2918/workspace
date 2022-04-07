8b0db76acb1ff8184f79c652159f1a2a8cf36e2a
mylove2918 <dnflwlq86@naver.com>
2022-04-07 18:25:34 +0900

    game start

---
+++ coding/code/js/game.js
@@ -0,0 +1,56 @@
+const key = {
+    keyDown : {},
+    keyValue : {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down',
+        88: 'attack'
+    }
+}
+
+const gameProp = {
+    screenWidth : window.innerWidth,
+    screenHeight : window.innerHeight
+
+}
+
+const renderGame = () => {
+    hero.keyMotion();
+    window.requestAnimationFrame(renderGame);    
+}
+
+const windowEvent = () => {
+    // 키를 누를때
+    window.addEventListener('keydown', e => {        
+        key.keyDown[key.keyValue[e.which]] = true;
+        // console.log(key.keyDown)
+        // console.log(e.which)        
+    })
+    // 키를 뗄때
+    window.addEventListener('keyup', e => {
+        key.keyDown[key.keyValue[e.which]] = false;        
+    })
+}
+
+const loadImg = () => {
+    const preLoadImgSrc = ['../../lib/images/ninja_attack.png','../../lib/images/ninja_run.png','../../lib/images/ninja_idle.png']
+    preLoadImgSrc.forEach(arr => {
+        const img = new Image();
+        img.src = arr;
+    })
+}
+
+let hero;
+
+const init = () => {
+    hero = new Hero('.hero');
+    loadImg();
+    windowEvent();
+    renderGame();
+    console.log(hero.position());
+}
+
+window.onload = () => {
+    init();
+}
